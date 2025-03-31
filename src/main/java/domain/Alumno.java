45d6cb82960313ec4c5095b1dfd817e5e60a7884
Facundo Diez Forradellas <facundo.diez.f@gmail.com>
2025-03-31 19:52:43 -0300

    Add fields, constructor, and methods to Alumno class

--- src/main/java/domain/Alumno.java
+++ src/main/java/domain/Alumno.java
@@ -1,4 +1,27 @@
 package domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Alumno {
+    @Setter  @Getter private String nombre;
+    @Setter  @Getter private String apellido;
+    @Getter List<Materia> materiasAprobadas;
+
+    //------------- Constructor
+    public Alumno(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.materiasAprobadas = new ArrayList<>();
+    }
+
+    //------------- Methods
+    public void agregarMateriasAprobadas(Materia ... materias){
+        Collections.addAll(materiasAprobadas, materias);
+    }
+
 }
